71cb94a9d60d65652732d9385eeadc8190410195
Toby-Heigle <toby.heigle07@gmail.com>
2025-06-16 16:52:00 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -25,13 +25,21 @@ var init = function (window) {
         physikz.addRandomVelocity(circle, canvas, 5, 5);
         view.addChild(circle);
         circles.push(circle);
-        
+        game.checkCirclePosition;
         
     }
     // TODO 3 : Call the drawCircle() function
+    // drawCircle([0]);
+    // drawCircle([1]);
+    // drawCircle([2]);
+    // drawCircle([3]);
+    // drawCircle([4]);
+    
 
     // TODO 7 : Use a loop to create multiple circles
-
+for (var i = 0; i < 25; i++){
+  drawCircle(100);
+}
     ///////////////////
     // PROGRAM LOGIC //
     ///////////////////
@@ -43,10 +51,23 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the position of each circle using physikz.updatePosition()
+      // physikz.updatePosition([0]);
+      // physikz.updatePosition([1]);
+      // physikz.updatePosition([2]);
+      // physikz.updatePosition([3]);
+      // physikz.updatePosition([4]);
       // TODO 5 : Call game.checkCirclePosition() on your circles
+      // game.checkCirclePosition(0);
+      // game.checkCirclePosition(1);
+      // game.checkCirclePosition(2);
+      // game.checkCirclePosition(3);
+      // game.checkCirclePosition(4);
       // TODO 8 / TODO 9 : Iterate over the array
     }
-
+    for (var i = 0; i < circles.length; i++){
+physikz.updatePosition(circles[i]);
+game.checkCirclePosition(circles[i]);
+    }
     /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -59,7 +80,15 @@ var init = function (window) {
       }
 
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-
+if (circle.x < 0){
+  circle.x =0;
+}
+if (circle.y < 0){
+  circle.y =0;
+}
+if (circle.y > canvas.height){
+  circle.y =0;
+}
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
     };
 
